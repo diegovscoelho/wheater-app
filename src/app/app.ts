09ae04279b51d266155994877fad750db2a88f51
diegovscoelho <dcoelhocarneiro@gmail.com>
2025-10-03 17:34:34 -0300

    Moving folders and developing the UI

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('wheater-app');
+
+  isDropdownOpen: boolean = false; 
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen; 
+  }
 }
